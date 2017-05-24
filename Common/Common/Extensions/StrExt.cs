f9ffa2704394de3f672ade0a25b5a0eeab5069e6
williamcui <williamcui8766@hotmail.com>
2017-05-25 00:17:09 +0800

    string ext IsNullOrEmpty

---
+++ Common/Common/Extensions/StrExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class StrExt
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
