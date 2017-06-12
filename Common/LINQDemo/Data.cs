9460ee7ba4ca2979c4e03f6bfad708f3c4e02f91
williamcui <williamcui8766@hotmail.com>
2017-06-13 00:02:21 +0800

    LINQ inner join

---
+++ Common/LINQDemo/Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    public static class Data
+    {
+        public static List<Person> GetPersonlist()
+        {
+            var list = new List<Person>
+            {
+                new Person {id =1 , Name = "Peter1" },
+                new Person {id =2 , Name = "Peter2" },
+                new Person {id =3 , Name = "Peter3" },
+                new Person {id =4 , Name = "Peter4" },
+            };
+            return list;
+        }
+
+        public static List<Pet> GetPetlist()
+        {
+            var list = new List<Pet>
+            {
+                new Pet {id =3 , Name = "Peter3",Owner="test1" },
+                new Pet {id =4 , Name = "Peter2",Owner="test2" },
+                new Pet {id =5 , Name = "Peter3",Owner="test3" },
+                new Pet {id =6 , Name = "Peter4",Owner="test4" },
+            };
+            return list;
+        }
+    }
+
+    public class Person
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Pet
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+    }
+   
+}
---
+++ Common/LINQDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var src1 = Data.GetPersonlist();
+            var src2 = Data.GetPetlist();
+
+            var res = (from c in src1
+                       join d in src2
+                       on new { ID = c.id, Name = c.Name }
+                       equals new { ID = d.id, Name = d.Name }
+                       select new { ID = d.id, Name = d.Name, Owner = d.Owner }).ToList();
+            foreach (var item in res)
+            {
+                Console.WriteLine("{0},{1},{2}",item.ID,item.Name,item.Owner);
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
